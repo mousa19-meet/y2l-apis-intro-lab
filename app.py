9aae3e7a743f01843b6b070e340e90d71581da7a
aboud20-meet <aboud20@meet.mit.edu>
2018-12-16 15:39:55 +0200

    dead

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def movies():
                     "image_url": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/KXC1W2-920x584.jpg"
                     }
                     """
-    return render_template('movie.html', movie={})
+    parsed_json = json.loads(json_string)
+    return render_template('movie.html', movie=parsed_json)
 
 
 @app.route('/tvshows')
@@ -55,10 +56,8 @@ def tv_shows():
     ]
     }]    
     """
-    # Write code here to take the `json_string` and return list of movies to the user
-
-
-    return render_template('tv_shows.html')
+    tv_shows = json.loads(json_string)
+    return render_template('tv_shows.html',tv_shows=tv_shows)
 
 
 ############################
@@ -74,7 +73,10 @@ def dog_breeds():
     Do a GET request to the link above to get all dog breeds and return them
     to them as a list to the user as a bullet pointed list
     """
-    return render_template('dogs.html')
+    response = requests.get("https://dog.ceo/api/breeds/list/all")
+    parsed_content = json.loads(response.content)
+    doggo = parsed_content["message"]
+    return render_template('dogs.html', doggo = doggo)
 
 if __name__ == '__main__':
     app.run(debug=True)
